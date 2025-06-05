ca6986d9cc82e1850a8132ec75c2c45f1057f5ed
Henrik Larsson_c <henrik.larsson_c@sandvik.com>
2025-06-05 12:41:52 +0200

    updated contracts 🤔

--- src/FooBar.API/Application/Contracts/Requests/AbortWeighingOutIncomingApiRequest.cs
+++ src/FooBar.API/Application/Contracts/Requests/AbortWeighingOutIncomingApiRequest.cs
@@ -2,9 +2,5 @@
 
 public sealed class AbortWeighingOutIncomingApiRequest : RedirectBaseRequest
 {
-    public required Guid ProductionOrderReferenceId { get; init; }
-    public required string DryerId { get; init; }
-    public required string Grade { get; init; }
-    public required string SequenceNumber { get; init; }
-    public required string MillBatch { get; init; }
+    public required Guid DryerId { get; init; }
 }
